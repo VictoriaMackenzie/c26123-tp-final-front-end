55fd5519cdda6e68bcf5b6c6331308b5e983c2ff
VictoriaMackenzie <victoriapatriciamackenzie@gmail.com>
2026-07-06 09:02:51 -0300

    Subo proyecto del tp final del curso de front enf con js.

---
+++ js/index.js
@@ -0,0 +1,55 @@
+//Importo las funciones para enviar objetos al array
+import { agregarAlCarrito } from "./funcionesCarrito.js";
+import { obtenerCarrito } from "./storage.js";
+import { actualizarContador } from "./ui.js";
+
+
+const renderizarProductos = () =>{
+
+    const contendedor = document.getElementById("contenedor-tarjetas");
+
+    fetch("./data/productos.json")
+    .then((response)=> response.json())
+    .then((data)=>
+    data.forEach((producto)=>{
+const tarjeta= document.createElement("article");
+tarjeta.classList.add("card", "text-dark");
+
+const img= document.createElement("img");
+img.src = `./${producto.img}`;
+        img.alt = producto.nombre;
+
+        const titulo = document.createElement("h3");
+        titulo.textContent = producto.nombre;
+
+        const precio = document.createElement("p");
+        precio.textContent = `$${producto.precio}`;
+
+        const descripcion = document.createElement("h5");
+        descripcion.textContent = producto.descripcion;
+
+const boton = document.createElement("button");
+boton.classList.add("btn", "bg-secondary", "text-dark");
+boton.textContent="Agregar al carrito";
+
+boton.addEventListener("click", ()=>{
+agregarAlCarrito(producto);
+});
+
+tarjeta.appendChild(img);
+tarjeta.appendChild(titulo);
+tarjeta.appendChild(descripcion);
+tarjeta.appendChild(precio);
+tarjeta.appendChild(boton);
+
+contendedor.appendChild(tarjeta);
+    }),
+     )
+.catch((error)=>console.log(error));
+};
+
+document.addEventListener("DOMContentLoaded", ()=>{
+const carrito= obtenerCarrito();
+actualizarContador(carrito);
+renderizarProductos();
+});
